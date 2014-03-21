b584921cbf0c05d4c9feb8ef4b679fc3d427adc9
Attsun <atsumi.tatsuya@gmail.com>
2014-03-21 12:53:31 +0900

    add favorite_count

--- test/TwitterSpec.hs
+++ test/TwitterSpec.hs
@@ -20,5 +20,5 @@ spec = do
     it "statuses/userTimeline API result" $ do
       let maxId = 446511275204296705
           message = "このイラスト、なんでみんな片目が吹っ飛んでるんだろう。｜国が違うとこんなに違う。各国別擬音の違い http://t.co/g53WQ4V6h6 @karapaiaさんから"
-      expect <- return $ [UserTimeline maxId message 0]
+      expect <- return $ [UserTimeline maxId message 0 1]
       userTimeline [("screen_name", "__Attsun__"), ("count", "1"), ("max_id", B.pack $ show maxId)] `shouldReturn` expect
